a102cba345d040576ff46b72d2ee7d79689f348c
andyrrr <43052958+andyrrr@users.noreply.github.com>
2023-10-01 02:39:00 -0600

    compilatorio

---
+++ Tarea1/Decodificador.sv
@@ -0,0 +1,14 @@
+module Decodificador(
+    input wire A, B, C,
+    output wire a, b, c, d, e, f, g, m);
+	 
+
+    assign a = ( (!A && B) || (!B && !C) || (A && C) );
+    assign b = (!A || !C);
+    assign c = (!B || !C || A);
+    assign d = ( (!A && !C) || (B && C) || (A && C) );
+    assign e = ( (!A && !B && !C) || (!A && B && C) || (A && !B && C));
+    assign f = ((A && B) || (A && C) || (!A && !B && !C));
+    assign g = (B || (A && C));
+	 assign m = ( (B && C));
+endmodule
---
+++ Tarea1/output_files/Combinatorio.cdf
@@ -0,0 +1,15 @@
+/* Quartus Prime Version 20.1.1 Build 720 11/11/2020 SJ Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Ign)
+		Device PartName(SOCVHPS) MfrSpec(OpMask(0));
+	P ActionCode(Cfg)
+		Device PartName(5CSEBA6U23) Path("C:/Users/maxta/Documents/GitHub/a_cordero_compu_archi_found_2G1_2023/Tarea1/output_files/") File("Combinatorio.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
