dcc5c5e57b4145e97f08797c2983bbc8ae6c7055
andyrrr <43052958+andyrrr@users.noreply.github.com>
2023-10-29 22:10:24 -0600

    estructural

--- Proyecto Grupal/Taller3/slave/PWM_Controller.sv
+++ Proyecto Grupal/Taller3/slave/PWM_Controller.sv
@@ -5,13 +5,14 @@ module PWM_Controller (
 );
 
 reg[7:0] counter = 0;
+wire reset_counter;
 
-always@(posedge clock) begin 
-	if (counter < 10) counter <= counter +1;
-	else counter <=0;
+assign reset_counter = (counter == 10'b1111) ? 1'b1 : 1'b0;
+
+always @(posedge clock or posedge reset_counter) begin
+    counter <= reset_counter ? 8'b0000_0000 : counter + 1;
 end
 
-assign led = (counter< (switches[0] + switches[1] *2 + switches[2] * 4 + switches[3] * 8) ) ? 1:0;
+assign led = (counter < (switches[0] + switches[1] * 2 + switches[2] * 4 + switches[3] * 8)+4) ? 1'b1 : 1'b0;
 
 endmodule
-
